e6838d942c066e2fb5b0e9530e3ccde10bec33b2
Aditya Satrya <aditya.satrya@gmail.com>
2020-12-13 13:12:43 +0700

    Update ctci-array-left-rotation.py

--- arrays/ctci-array-left-rotation.py
+++ arrays/ctci-array-left-rotation.py
@@ -8,19 +8,19 @@ import sys
 
 # Complete the rotLeft function below.
 def rotLeft(a, d):
-    print("a={}".format(a))
+    #print("a={}".format(a))
     
     new_a = [None for i in a]
-    print("new_a={}".format(new_a))
+    #print("new_a={}".format(new_a))
     min_rotation = d % len(a)
-    print("min_rotation={}".format(min_rotation))
+    #print("min_rotation={}".format(min_rotation))
     
     idx = 0
     for v in a:
         new_idx = idx-min_rotation if idx-min_rotation >=0 else len(a)+(idx-min_rotation)
-        print("{} to {}".format(idx, new_idx))
+        #print("{} to {}".format(idx, new_idx))
         new_a[new_idx]= a[idx]
-        print("new_a={}".format(new_a))
+        #print("new_a={}".format(new_a))
         idx = idx + 1
         
     return new_a
